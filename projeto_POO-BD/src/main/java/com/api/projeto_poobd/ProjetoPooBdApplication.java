c6fad31893cf1ff0a021ec36ce6f670e1281881e
arthur.ngdi <arthur.f@gec.inatel.br>
2022-06-15 21:32:52 -0300

    first commit

---
+++ projeto_POO-BD/src/main/java/com/api/projeto_poobd/ProjetoPooBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_poobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPooBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPooBdApplication.class, args);
+	}
+
+}
---
+++ projeto_POO-BD/src/test/java/com/example/projeto_poobd/ProjetoPooBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_poobd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPooBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
