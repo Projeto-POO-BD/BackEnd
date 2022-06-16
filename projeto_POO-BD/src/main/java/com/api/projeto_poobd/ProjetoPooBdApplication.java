20d55dac0bc5af1dcb28e2ec4d9d676a29dfbfb7
arthur.ngdi <arthur.f@gec.inatel.br>
2022-06-16 12:31:53 -0300

    Exclusão da pasta resources do .gitignore

--- projeto_POO-BD/src/main/java/com/api/projeto_poobd/ProjetoPooBdApplication.java
+++ projeto_POO-BD/src/main/java/com/api/projeto_poobd/ProjetoPooBdApplication.java
@@ -1,9 +1,11 @@
-package com.example.projeto_poobd;
+package com.api.projeto_poobd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjetoPooBdApplication {
 
 	public static void main(String[] args) {
